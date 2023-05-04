be5d4cb3e8330b8c5e14606289900a94e212f974
0xdanny <dpaolone03@gmail.com>
2023-05-04 15:30:16 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Inference_Engine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
